f48e6ccbcfaab4567794f76aaaf492c1969c8d7f
nissi-devo <>
2022-12-08 01:38:06 +0100

    Added User Auth

--- expressprod/urls.py
+++ expressprod/urls.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
+
+    #Django Admin
     path("admin/", admin.site.urls),
+
+    #User management
+    path("accounts/", include("django.contrib.auth.urls")),
+
+    #Local app
     path("", include("store.urls"))
+
+    
 ]
